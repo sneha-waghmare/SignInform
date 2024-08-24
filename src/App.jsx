1eed81db4269935647849c0ce994c1642ffb7f06
waghmare sneha <waghmaresnehuu@gmail.com>
2024-08-24 20:12:35 +0530

    some changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+function App() {
+  return (
+    <div className='text-red-500'>hello world</div>
+  );
+}
+
+export default App;
