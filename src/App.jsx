6b8c2ef94630a6dac2cae81a2398287094c542cd
waghmare sneha <waghmaresnehuu@gmail.com>
2024-08-24 22:22:43 +0530

    change the color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import react from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './index.css'
+
 function App() {
   return (
-    <div className='text-red-500'>hello world</div>
+    <div className=' h-screen w-full text-center '>
+      <h1 text-blue-300>hello world</h1>
+    </div>
   );
 }
 
