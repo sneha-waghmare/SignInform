fe871db3506fd84962fb2cc54e5a832dbde54a69
waghmare sneha <waghmaresnehuu@gmail.com>
2024-08-25 23:49:30 +0530

    make form responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
-    <div className=' h-screen w-full bg-pink-500 text-center content-center'>
-      <div className="h-2/3 w-2/5 mx-auto bg-white rounded-3xl text-center py-10">
-      <h1 className="text-2xl m-6 text-black font-bold">SIGN IN
+    <div className=' h-screen w-full bg-pink-500 content-center'>
+      <div className="sm:h-full sm:w-full sm:pl-4 pl-1 pr-1 mx-auto bg-white rounded-3xl py-10 xs:w-full h-full md:w-3/4 md:h-3/4 lg:w-2/4 lg:h-3/4">
+      <h1 className="text-center text-2xl m-6 text-black font-bold ">SIGN IN
       
       </h1>
         <form action="#">
-          <label className="text-2xl text-gray-500">  Email : 
-            <input type="email" required placeholder="✉ Email" className="border-2 w-2/3 py-2 m-6 rounded-md " />
+          <label className="text-2xl text-gray-500 ">  Email : <br />
+            <input type="email" required placeholder="✉ Email" className="border-2 w-2/3 py-2 m-6 rounded-md lg:m-3" />
           </label> 
-          
-          <label className="text-2xl text-gray-500">Password : 
-            <input type="password" required placeholder="🔒Password " className="border-2 w-2/3 py-2 m-6 rounded-md text-gray-700" />
+          <br />
+          <label className="text-2xl text-gray-500">Password : <br />
+            <input type="password" required placeholder="🔒Password " className="border-2 w-2/3 py-2 m-6 rounded-md text-gray-700 lg:m-3" />
           </label>
           <br />
           <label htmlFor="" className="text-black w-full mr-20">
           <input type="checkbox" required className="m-2 text-gray-500 text-left" /> Remember Me
           </label>
-          <a href="#" className="text-right border ml-16 text-blue-400 font-bold hover:text-red-400 active:text-black ">Forgot Password</a>
+          <a href="#" className="text-right border ml-5 text-blue-400 font-bold hover:text-red-400 active:text-black xs:block sm:ml-20 lg:mr-10">Forgot Password</a>
           
-          <label className="m-10">
+          <label className="m-10 w-full sm:pl-4">
             
-            <input type="submit" value="Login" className="w-2/4 py-3 mt-16  text-white bg-blue-500 rounded-md hover:bg-blue-400 active:bg-white" />  {/* Button */}
+            <input type="submit" value="Login" className="py-3 m-auto mt-24  text-white bg-blue-500 rounded-md hover:bg-blue-400 active:bg-white xs:w-2/4 sm:w-3/4 lg:w-3/4 lg:ml-7 lg:m-6" />  {/* Button */}
             </label>
         </form>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '500px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      screens: {
+        'xs':'320px',
+        // => @media (min-width: 992px) { ... }
+      },
+    },
   },
   plugins: [],
 }
